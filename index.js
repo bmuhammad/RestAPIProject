89cff6a3671e44284e9f2ed6a974caf8e9325025
Brian <briancmuhammad@yahoo.com>
2024-09-30 06:56:09 -0700

    separated data out into a repository

--- index.js
+++ index.js
@@ -2,13 +2,22 @@
 let express = require("express");
 let app = express();
 
+//Assign variable for accessing data. Data is in an array.
+let satelliteRepo = require("./repos/satelliteRepo");
+
 //Use the express Router opbject
 let router = express.Router();
+let satellites = satelliteRepo.get();
 
 //Create GET to return a list of satellites
 router.get("/", function (req, res, next) {
-    res.send("communication");
+    res.status(200).json({
+     "status": 200,
+     "sttusText": "OK",
+     "message": "All satellites retrieved.",
+     "data": satellites
   });
+});
 
 
 //Configure router so all routes are prefixed /api/v1
